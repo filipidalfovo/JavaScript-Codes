853874fed4eb2e30b1331537abc4e2966fca3619
lipe-av <filipi.dalfovo@gmail.com>
2022-09-08 11:25:47 -0300

    Create lettercounter.js

---
+++ lettercounter.js
@@ -0,0 +1,15 @@
+const breakText = (fullString) => {
+
+    let objLetter = {}
+
+    for (let letter of fullString) {
+        if (objLetter.hasOwnProperty(letter)) {
+            objLetter[letter] = objLetter[letter] + 1
+        } else {
+            objLetter[letter] = 1
+        }
+    }
+    return objLetter
+}
+
+console.log(breakText('Insira o seu texto aqui'))
